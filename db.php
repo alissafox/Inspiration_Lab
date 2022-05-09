1d15d3512e58750c2bf6a8a343c1f3185fc01700
Mona  Lisa <mona.lisa@louvre.fr>
2022-05-09 19:54:42 +0200

    tt

--- db.php
+++ db.php
@@ -12,38 +12,42 @@
 //   }//else{
 // //     echo "hey wellcome";
 // //     }
-    ?>
+?>
     <?php
 
-class Database {
-  private  $hostname = "localhost";
-  private  $username = "root";
-  private  $password = "root";
-  private  $database = "astrograma";
-  public $conn;
+    class Database
+    {
+      private  $hostname = "localhost";
+      private  $username = "root";
+      private  $password = "root";
+      private  $database = "astrograma";
+      public $conn;
 
-  //constructor
-  public function __construct(){
-   $this->conn = $this->getConnection();
-
-  }
-  //methode
-  private function getConnection(){
-      $connection = mysqli_connect($this->hostname,$this->username,$this->password,$this->database);
-    if($connection == false){
-        echo  "no connection";
-        die();
+      //constructor
+      public function __construct()
+      {
+        $this->conn = $this->getConnection();
+      }
+      //methode
+      private function getConnection()
+      {
+        $connection = mysqli_connect($this->hostname, $this->username, $this->password, $this->database);
+        if ($connection == false) {
+          echo  "no connection";
+          die();
+        } else {
+          echo "woooooooooo";
+          return $connection;
+        };
+      }
+      // select query
+      public function selectQuery($sql)
+      {
+        return  mysqli_query($this->conn, $sql)->fetch_all(MYSQLI_ASSOC);
+      }
+      //isert data query
+      public function insertQuery($sql)
+      {
+        return  mysqli_query($this->conn, $sql);
+      }
     }
-   
-    return $connection;
-  }
-// select query
-  public function selectQuery($sql){
-      return  mysqli_query($this->conn, $sql)->fetch_all(MYSQLI_ASSOC);
-  }
-//isert data query
-  public function insertQuery($sql){
-    return  mysqli_query($this->conn, $sql);
-
-  }
-}
