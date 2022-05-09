12cd1deb4f66867b779b46c4a91eb265d474848e
alissafox <alexworkhf@gmail.com>
2022-05-09 20:07:33 +0200

    c

--- sign_up.php
+++ sign_up.php
@@ -16,7 +16,7 @@
 
     <h2> SignUp Page</h2><br>
     <div class="log_in">
-        <form id="logIn" method="POST" action="success.php">
+        <form id="logIn" method="POST" action="profile_page.html">
             <label><b>First Name</b></label>
             <input type="text" name="firstName" id="firstName" placeholder="Firstname">
             <br><br>
@@ -36,6 +36,7 @@
             <label><b>Password</b></label>
             <input type="Password" name="Pass" id="Pass" placeholder="Password">
             <br><br>
+            <label><b>Submit</b></label>
             <input type="submit">
             <br><br>
             <input type=" checkbox" id="check">
